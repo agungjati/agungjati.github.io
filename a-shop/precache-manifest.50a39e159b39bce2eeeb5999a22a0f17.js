ed2214153c6d31ebd7dd400d9384f7409837e815
agung jati <agungjati94@gmail.com>
2020-10-10 21:15:23 +0700

    adding a shop

---
+++ a-shop/precache-manifest.50a39e159b39bce2eeeb5999a22a0f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d35014dfd7b291e081734bc01e36a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a470802b97bb10cc81",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cccf6a6688b3b73f353",
+    "url": "/static/js/2.0fe11dfe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fe11dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a470802b97bb10cc81",
+    "url": "/static/js/main.33965d15.chunk.js"
+  },
+  {
+    "revision": "1c6e1dcbb9d0cb703b9a",
+    "url": "/static/js/runtime-main.6733019a.js"
+  }
+]);
